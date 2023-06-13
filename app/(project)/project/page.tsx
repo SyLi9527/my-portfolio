fd925f4fa40c2a3892684544b3ddb3382ca8f449
SyLi_4aaf <shaoyan.li@uzh.ch>
2023-06-13 03:47:56 +0200

    Style: change classname order

--- app/(project)/project/page.tsx
+++ app/(project)/project/page.tsx
@@ -5,24 +5,15 @@ export default function Page() {
         <>
             <div className="flex h-screen items-center justify-center">
                 <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-center text-3xl font-bold text-slate-800">
+                    <h1 className="text-slate-800 text-center text-3xl font-bold">
                         Hi, there!
                     </h1>
-                    <h2 className="text-center text-2xl font-bold text-slate-800">
+                    <h2 className="text-slate-800 text-center text-2xl font-bold">
                         This is the project page.
                     </h2>
                 </div>
             </div>
-            <div className="flex h-screen items-center justify-center">
-                <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-center text-3xl font-bold text-slate-800">
-                        Hi, there!
-                    </h1>
-                    <h2 className="text-center text-2xl font-bold text-slate-800">
-                        This is the project page.
-                    </h2>
-                </div>
-            </div>
+           
         </>
     )
 }
